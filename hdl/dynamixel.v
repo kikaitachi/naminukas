6c06f8c5a50e019cec3d4f3910c1aa21496e6cf2
test <61438920+kikaitachi@users.noreply.github.com>
2021-08-30 16:25:05 +0100

    Simplify code with macro

--- hdl/dynamixel.v
+++ hdl/dynamixel.v
@@ -72,10 +72,22 @@ initial begin
     crc = 0;
 end
 
+`define dynamixel_send_byte(BYTE) \
+begin \
+    if (done == 1) begin \
+        send_byte <= 1; \
+        byte_to_send <= BYTE; \
+        crc <= crc16(BYTE, crc); \
+        state <= state + 1; \
+    end else begin \
+        send_byte <= 0; \
+    end \
+end
+
 always @(posedge clock)
 begin
     case (state)
-        // Header 1
+        // Header
         0: begin
             if (send == 1) begin
                 send_byte <= 1;
@@ -84,347 +96,51 @@ begin
                 crc <= crc16(8'hFF, crc);
             end
         end
-        // Header 2
-        1: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'hFF;
-                state <= 2;
-                crc <= crc16(8'hFF, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // Header 3
-        2: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'hFD;
-                state <= 3;
-                crc <= crc16(8'hFD, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        1: `dynamixel_send_byte(8'hFF)
+        2: `dynamixel_send_byte(8'hFD)
         // Reserved
-        3: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'h00;
-                state <= 4;
-                crc <= crc16(8'h00, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        3: `dynamixel_send_byte(8'h00)
         // Packet ID
-        4: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'hFE;  // Broadcast ID
-                state <= 5;
-                crc <= crc16(8'hFE, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // Packet length: the least significant byte
-        5: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'h1B;
-                state <= 6;
-                crc <= crc16(8'h1B, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // Packet length: the most significant byte
-        6: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'h00;
-                state <= 7;
-                crc <= crc16(8'h00, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        4: `dynamixel_send_byte(8'hFE)  // Broadcast ID
+        // Packet length
+        5: `dynamixel_send_byte(8'h1B)  // The least significant byte
+        6: `dynamixel_send_byte(8'h00)  // the most significant byte
         // Instruction
-        7: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'h83;  // Sync Write
-                state <= 8;
-                crc <= crc16(8'h83, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // Address: the least significant byte
-        8: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= address[7:0];
-                state <= 9;
-                crc <= crc16(8'h74, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // Address: the most significant byte
-        9: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= address[15:8];
-                state <= 10;
-                crc <= crc16(8'h00, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // Data length: the least significant byte
-        10: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'h04;
-                state <= 11;
-                crc <= crc16(8'h04, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // Data length: the most significant byte
-        11: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= 8'h00;
-                state <= 12;
-                crc <= crc16(8'h00, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        7: `dynamixel_send_byte(8'h83)  // Sync Write
+        // Address
+        8: `dynamixel_send_byte(address[ 7:0])  // The least significant byte
+        9: `dynamixel_send_byte(address[15:8])  // The most significant byte
+        // Data length
+        10: `dynamixel_send_byte(8'h04)  // The least significant byte
+        11: `dynamixel_send_byte(8'h00)  // The most significant byte
         // 1st device ID
-        12: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= id1;
-                state <= 13;
-                crc <= crc16(id1, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value1: byte 1
-        13: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value1[7:0];
-                state <= 14;
-                crc <= crc16(value1[7:0], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value1: byte 2
-        14: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value1[15:8];
-                state <= 15;
-                crc <= crc16(value1[15:8], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value1: byte 3
-        15: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value1[23:16];
-                state <= 16;
-                crc <= crc16(value1[23:16], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value1: byte 4
-        16: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value1[31:24];
-                state <= 17;
-                crc <= crc16(value1[31:24], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        12: `dynamixel_send_byte(id1)
+        // value1
+        13: `dynamixel_send_byte(value1[ 7: 0])  // byte 1
+        14: `dynamixel_send_byte(value1[15: 8])  // byte 2
+        15: `dynamixel_send_byte(value1[23:16])  // byte 3
+        16: `dynamixel_send_byte(value1[31:24])  // byte 4
         // 2nd device ID
-        17: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= id2;
-                state <= 18;
-                crc <= crc16(id2, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value2: byte 1
-        18: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value2[7:0];
-                state <= 19;
-                crc <= crc16(value2[7:0], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value2: byte 2
-        19: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value2[15:8];
-                state <= 20;
-                crc <= crc16(value2[15:8], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value2: byte 3
-        20: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value2[23:16];
-                state <= 21;
-                crc <= crc16(value2[23:16], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value2: byte 4
-        21: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value2[31:24];
-                state <= 22;
-                crc <= crc16(value2[31:24], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        17: `dynamixel_send_byte(id2)
+        // value2
+        18: `dynamixel_send_byte(value2[ 7: 0])  // byte 1
+        19: `dynamixel_send_byte(value2[15: 8])  // byte 2
+        20: `dynamixel_send_byte(value2[23:16])  // byte 3
+        21: `dynamixel_send_byte(value2[31:24])  // byte 4
         // 3rd device ID
-        22: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= id3;
-                state <= 23;
-                crc <= crc16(id3, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value3: byte 1
-        23: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value3[7:0];
-                state <= 24;
-                crc <= crc16(value3[7:0], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value3: byte 2
-        24: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value3[15:8];
-                state <= 25;
-                crc <= crc16(value3[15:8], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value3: byte 3
-        25: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value3[23:16];
-                state <= 26;
-                crc <= crc16(value3[23:16], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value3: byte 4
-        26: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value3[31:24];
-                state <= 27;
-                crc <= crc16(value3[31:24], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        22: `dynamixel_send_byte(id3)
+        // value3
+        23: `dynamixel_send_byte(value3[ 7: 0])  // byte 1
+        24: `dynamixel_send_byte(value3[15: 8])  // byte 2
+        25: `dynamixel_send_byte(value3[23:16])  // byte 3
+        26: `dynamixel_send_byte(value3[31:24])  // byte 4
         // 4th device ID
-        27: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= id4;
-                state <= 28;
-                crc <= crc16(id4, crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value4: byte 1
-        28: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value4[7:0];
-                state <= 29;
-                crc <= crc16(value4[7:0], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value4: byte 2
-        29: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value4[15:8];
-                state <= 30;
-                crc <= crc16(value4[15:8], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value4: byte 3
-        30: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value4[23:16];
-                state <= 31;
-                crc <= crc16(value4[23:16], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
-        // value4: byte 4
-        31: begin
-            if (done == 1) begin
-                send_byte <= 1;
-                byte_to_send <= value4[31:24];
-                state <= 32;
-                crc <= crc16(value4[31:24], crc);
-            end else begin
-                send_byte <= 0;
-            end
-        end
+        27: `dynamixel_send_byte(id4)
+        // value4
+        28: `dynamixel_send_byte(value4[ 7: 0])  // byte 1
+        29: `dynamixel_send_byte(value4[15: 8])  // byte 2
+        30: `dynamixel_send_byte(value4[23:16])  // byte 3
+        31: `dynamixel_send_byte(value4[31:24])  // byte 4
         // CRC: the least significant byte
         32: begin
             if (done == 1) begin
