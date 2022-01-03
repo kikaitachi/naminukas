b7be87aee3b48f5d5beb9b9bc56eb0dcc69f2c0c
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2022-01-03 16:06:11 +0000

    feat: implement CRC calculation in uplink frame

--- hdl/fport_radio.v
+++ hdl/fport_radio.v
@@ -19,15 +19,14 @@ localparam FPORT_READ_BYTES                  = 2'b11;
 
 localparam half_clocks_per_bit = clocks_per_bit / 2;
 
-// If 20 zero bits are detected assume transmission is over
-localparam clocks_to_dectect_transmission_end = clocks_per_bit * 20;
-
 reg [1:0] state = FPORT_WAIT_FOR_TRANSMISSION_START;
 // TODO: figure how many bits are really needed based on clock_frequency
 reg [32:0] clocks_to_skip = 0;
 reg [2:0] byte_bit_index;
 reg [7:0] byte_index = 0;
 reg [7:0] received_byte;
+
+reg [7:0] channel0;
 
 reg fport_sending = 0;
 wire fport_output;
@@ -39,6 +38,7 @@ assign fport_input = fport;
 reg send_byte = 0;
 reg[7:0] byte_to_send;
 wire done;
+reg[7:0] crc;
 
 uart
 #(
@@ -54,6 +54,16 @@ uplink_uart
     .pin(fport_output)
 );
 
+function automatic [7:0] crc8;
+    input [7:0] crcIn;
+    input [7:0] data;
+    reg [8:0] sum;
+begin
+    sum = crcIn + data;
+    crc8 = sum[7:0] + {7'b0000000,sum[8]};
+end
+endfunction
+
 always @(posedge clock) begin
   if (clocks_to_skip != 0) begin
     clocks_to_skip <= clocks_to_skip - 1;
@@ -66,6 +76,7 @@ always @(posedge clock) begin
         byte_index <= 0;
         byte_to_send <= 'h08;
         send_byte <= 1;
+        crc <= crc8(crc, 'h08);
       end
       FPORT_RESPOND: begin
         if (done == 1) begin
@@ -73,44 +84,50 @@ always @(posedge clock) begin
             byte_index <= 1;
             byte_to_send <= 'h81;
             send_byte <= 1;
+            crc <= crc8(crc, 'h81);
           end else if (byte_index == 1) begin
             byte_index <= 2;
             byte_to_send <= 'h10;
             send_byte <= 1;
+            crc <= crc8(crc, 'h10);
           end else if (byte_index == 2) begin
             byte_index <= 3;
             byte_to_send <= 'h00;
             send_byte <= 1;
+            crc <= crc8(crc, 'h00);
           end else if (byte_index == 3) begin
             byte_index <= 4;
             byte_to_send <= 'h51;
             send_byte <= 1;
+            crc <= crc8(crc, 'h51);
           end else if (byte_index == 4) begin
             byte_index <= 5;
-            byte_to_send <= 'h01;
+            byte_to_send <= channel0;
             send_byte <= 1;
+            crc <= crc8(crc, channel0);
           end else if (byte_index == 5) begin
             byte_index <= 6;
             byte_to_send <= 'h00;
             send_byte <= 1;
+            crc <= crc8(crc, 'h00);
           end else if (byte_index == 6) begin
             byte_index <= 7;
             byte_to_send <= 'h00;
             send_byte <= 1;
+            crc <= crc8(crc, 'h00);
           end else if (byte_index == 7) begin
             byte_index <= 8;
             byte_to_send <= 'h00;
             send_byte <= 1;
+            crc <= crc8(crc, 'h00);
           end else if (byte_index == 8) begin
             byte_index <= 9;
-            byte_to_send <= 20;  // TODO: calculate CRC
+            byte_to_send <= 'hFF - crc;
             send_byte <= 1;
           end else begin
             fport_sending <= 0;
             state <= FPORT_WAIT_FOR_TRANSMISSION_START;
             byte_index <= 0;
-            //debug_pin <= 1;
-            //clocks_to_skip <= 36000;  // 3ms
           end
         end else begin
           send_byte <= 0;
@@ -121,7 +138,6 @@ always @(posedge clock) begin
           state <= FPORT_READ_BYTES;
           clocks_to_skip <= half_clocks_per_bit * 3;
           byte_bit_index <= 0;
-          //debug_pin <= 1;
         end
       end
       FPORT_READ_BYTES: begin
@@ -144,10 +160,14 @@ always @(posedge clock) begin
           end else if (byte_index == 40) begin
             state <= FPORT_WAIT_FOR_TRANSMISSION_END;
             clocks_to_skip <= clocks_per_bit * 16;
+            crc <= 0;
           end else begin
             state <= FPORT_WAIT_FOR_TRANSMISSION_START;
             byte_index <= byte_index + 1;
             clocks_to_skip <= clocks_per_bit;
+            if (byte_index == 3) begin
+              channel0 <= received_byte;
+            end
           end
         end else begin
           byte_bit_index <= byte_bit_index + 1;
